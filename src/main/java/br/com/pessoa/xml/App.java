cd06ae16ce79ca70ea758c81fc6d22f4de2b95cc
Fabio4209 <fabio4209@gmail.com>
2017-01-02 20:42:30 -0200

    organizando o código

--- src/main/java/br/com/pessoa/xml/App.java
+++ src/main/java/br/com/pessoa/xml/App.java
@@ -8,11 +8,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        PessoaFisica pessoaFisica = new PessoaFisica();
+        criarXMLUtilizandoSchema();
+    }
+
+    
+	private static void criarXMLUtilizandoSchema() {
+		PessoaFisica pessoaFisica = criarPessoaFisica();
+        JAXB.marshal(pessoaFisica, System.out);
+	}
+
+	private static PessoaFisica criarPessoaFisica() {
+		PessoaFisica pessoaFisica = new PessoaFisica();
         pessoaFisica.setNome("Fabio");
         pessoaFisica.setId(Long.valueOf(10));
         pessoaFisica.setCpf("689.300.381-15");
-        
-        JAXB.marshal(pessoaFisica, System.out);
-    }
+		return pessoaFisica;
+	}
 }
