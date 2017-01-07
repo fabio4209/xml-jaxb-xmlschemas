f34c8d214a3c09a142306f2dd0ccecd28ebdd266
Fabio4209 <fabio4209@gmail.com>
2017-01-07 19:48:38 -0200

    organização do código

--- src/main/java/br/com/pessoa/xml/entidade/ObjectFactory.java
+++ src/main/java/br/com/pessoa/xml/entidade/ObjectFactory.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.8-b130911.1802 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2016.12.31 às 10:47:44 AM BRST 
+// Gerado em: 2017.01.07 às 07:33:27 PM BRST 
 //
 
 
--- src/main/java/br/com/pessoa/xml/entidade/package-info.java
+++ src/main/java/br/com/pessoa/xml/entidade/package-info.java
@@ -2,8 +2,8 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.8-b130911.1802 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2016.12.31 às 10:47:44 AM BRST 
+// Gerado em: 2017.01.07 às 07:33:27 PM BRST 
 //
 
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.pessoa.com.br/xml/entidade", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.pessoa.com.br/xml/entidade")
 package br.com.pessoa.xml.entidade;
